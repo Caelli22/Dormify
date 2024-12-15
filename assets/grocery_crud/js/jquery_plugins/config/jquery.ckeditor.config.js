1da4f53a2873bba10c5e417704968d9fc7b46001
Dave Bryan Ico <davebryanico@gmail.com>
2024-12-15 20:29:08 +0800

    Updating

--- assets/grocery_crud/js/jquery_plugins/config/jquery.ckeditor.config.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	$( 'textarea.texteditor' ).ckeditor({
-		toolbar:'Full',
-		language: languageabbr
-	});
-	$( 'textarea.mini-texteditor' ).ckeditor({toolbar:'Basic',width:700});
-});
